1cfbfedf5d373837a05d270b80c14803e11877f7
Rajan Maurya <rajanmaurya154@gmail.com>
2019-11-07 18:57:47 +0530

    Fix: Convert test classes into kotlin

---
+++ Sweet-Error/src/androidTest/java/com/github/therajanmaurya/sweeterror/ExampleInstrumentedTest.kt
@@ -0,0 +1,28 @@
+package com.github.therajanmaurya.sweeterror
+
+import android.content.Context
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("com.github.therajanmaurya.sweeterror.test", appContext.packageName)
+    }
+}
