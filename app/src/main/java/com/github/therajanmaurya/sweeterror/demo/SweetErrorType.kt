219b86a53e448ccb61fbfaa95e8107f48085dbf4
Jayesh <kukadia.jayesh@gmail.com>
2020-12-17 13:00:48 +0530

    Update sample code to show custom sweet UI

--- app/src/main/java/com/github/therajanmaurya/sweeterror/demo/SweetErrorType.kt
+++ app/src/main/java/com/github/therajanmaurya/sweeterror/demo/SweetErrorType.kt
@@ -1,5 +1,5 @@
 package com.github.therajanmaurya.sweeterror.demo
 
 enum class SweetErrorType {
-    EMPTY_UI, ERROR_UI, NO_INTERNET, CUSTOM
+    EMPTY_UI, ERROR_UI, NO_INTERNET, CUSTOM, CUSTOM_STYLE
 }
